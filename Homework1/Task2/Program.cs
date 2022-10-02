9202b55758cc25b265764935e61f75c16b477392
ярослав переверзев <yar1234567@mail.ru>
2022-10-02 09:49:37 +0300

    Task2

---
+++ Homework1/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите два числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+{
+    Console.WriteLine("Первое число" + " " + num1 + " " + "больше чем второе" + " " + num2);
+}
+else
+{
+    Console.WriteLine("Второе число" + " " + num2 + " " + "больше чем первое" + " " + num1);
+}
