024d7a316a10863b1926678f4b2d27539835853e
ярослав переверзев <yar1234567@mail.ru>
2022-10-20 21:52:13 +0300

    Homework6

---
+++ Homework6/Task41/Program.cs
@@ -0,0 +1,29 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+void IntroArray(int[] number)
+{
+    for(int i = 0; i < number.Length; i++)
+    {
+        Console.Write("Введите число: ");
+        number[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int GetCount(int[] num)
+{
+    int count = 0;
+    for(int index = 0; index < num.Length; index++)
+    {
+        if(num[index] > 0)
+        {
+            count ++;
+        }
+    }
+    return count;
+}
+int [] array = new int [5];
+IntroArray(array);
+Console.WriteLine($"Пользователь ввел больше нуля {GetCount(array)} числа");
+
+
+
+
---
+++ Homework6/Task43/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+double GetMean(string text)
+{
+    Console.Write(text + ": ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+string CrossPoint (double b1, double b2, double k1, double k2)
+{
+    if (k1 == k2)
+    {
+        string point = ("прямые не пересекаются");
+        return point;
+    }
+    else
+    {
+        double x = (b1-b2)/(k2-k1);
+        double y = k1 * x + b1;
+        string point = ($"({x};{y})");
+        return point;
+    }
+}
+double b1 = GetMean("b1");
+double b2 = GetMean("b2");
+double k1 = GetMean("k1");
+double k2 = GetMean("k2");
+Console.WriteLine($"Точка пересечения двух прямых {CrossPoint(b1,b2,k1,k2)}");
