a61ad6375d216ece4e67b1c7a4a262fe35bc315d
ярослав переверзев <yar1234567@mail.ru>
2022-10-29 22:48:32 +0300

    Homework8

---
+++ Homework8/Task/Program.cs
@@ -0,0 +1,69 @@
+﻿// Есть число N. Скольно групп M, можно получить при разбиении всех чисел на группы, так чтобы в одной группе все числа были взаимно просты.
+//Например для N = 50, M получается 6. Задача: найти M при заданном N и получить одно из разбиений на группы N ≤ 10²⁰
+
+void CreateRows(int[] arrayCheck)
+{
+  int[] arrayTemp = new int[arrayCheck.Length];
+  int m = 1;
+  int count = 0;
+  int tempNumber = 0;
+  int tempNumber2 = 0;
+  int tempSwitch = 0;
+  
+  for (int i = 0; i < arrayCheck.Length; i++)
+  {
+    Array.Clear(arrayTemp);
+    count = 0;
+    if (arrayCheck[i] != 0)
+    {
+      arrayTemp[count] = arrayCheck[i];
+      tempNumber2 = arrayCheck[i];
+
+      for (int j = i; j < arrayCheck.Length; j++)
+      {
+        if (arrayCheck[j] % tempNumber2 != 0 || arrayCheck[j] / tempNumber2 == 1)
+        {
+          tempSwitch = 0;
+          tempNumber = arrayCheck[j];
+          for (int k = 0; k < count; k++)
+          {
+            if (tempNumber % arrayTemp[k] == 0) tempSwitch++;
+          }
+          if (tempSwitch == 0)
+          {
+            arrayTemp[count] = arrayCheck[j];
+            count++;
+            arrayCheck[j] = 0;
+          }
+        }
+      }
+      Console.WriteLine($"Группа {m++}: {PrintIntArray(arrayTemp)}");
+    }
+  }
+}
+
+int[] CreateArray(int n)
+{
+  int[] temp = new int[n];
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = i + 1;
+  }
+  return temp;
+}
+
+string PrintIntArray(int[] array)
+{
+  string result = string.Empty;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] != 0) result += $"{array[i]} ";
+  }
+  return result;
+}
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] tempArray = CreateArray(n);
+CreateRows(tempArray);
---
+++ Homework8/Task56/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+void FillArray(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0,10);
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void NumberRow(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            sumRow += array[i, j];
+        }
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"Строка {minSumRow + 1} с наименьшей суммой элементов {minRow}.");
+}
+int [,] matrix = new int [4,4];
+FillArray(matrix);
+NumberRow(matrix);
